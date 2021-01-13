477d2ba62932d58da4b74dc8ee44ef325392dc54
nishant4292 <77281504+nishant4292@users.noreply.github.com>
2021-01-13 21:55:22 +0530

    content changes

--- exception/TestException.java
+++ exception/TestException.java
@@ -2,7 +2,7 @@ package com.raystech.exception;
 public class TestException {
 	
 	 public static void main(String[] args) {
-		 try { String s= "nishant";
+		 try { String s= "naveen";
 		          System.out.println(s.charAt(8));
 	 }catch(StringIndexOutOfBoundsException e) {
 		 System.out.println("character kam hai");
